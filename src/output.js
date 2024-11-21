815984943065790fa2bdb8cc58af96f6cf388cb6
Haya <Haya.A.S.Alqassem@gmail.com>
2024-11-21 13:23:26 +0300

    Update output.js

--- src/output.js
+++ src/output.js
@@ -1,11 +1,6 @@
-/* 
-generateTextResult should return a text that is ['Result: ' + calculationResult] if it receives a number.
-calculationResult is the number as a text. 
-*/
 export function generateTextResult(calculationResult) {
   let textResult = '';
 
-  // 'invalid' and 'no-calc' are error codes, if calculationResult does not hold a number output. 
   if (calculationResult === 'invalid') {
     textResult = 'Invalid input. You must enter valid numbers.';
   } else if (calculationResult !== 'no-calc') {
@@ -17,7 +12,7 @@ export function generateTextResult(calculationResult) {
 
 export function outputResult(textResult) {
     const output = document.getElementById('result'); 
-    output.textContent = textResult; // Access the DOM to output this textResult. 
+    output.textContent = textResult; 
 }
 
 
