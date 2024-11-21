be21372cb3ffebe70bc11da6cc2493eefda04405
Haya <Haya.A.S.Alqassem@gmail.com>
2024-11-21 13:19:55 +0300

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,4 @@
-// This file is dealing with parsing the users input. 
-
 export function extractNumbers(formData) {
-  /*
-The extractNumbers function is using a feature that is built into the browser
-that is the formData object. 
-  */
   const num1Input = formData.get('num1');
   const num2Input = formData.get('num2');
 
@@ -12,10 +6,9 @@ that is the formData object.
 }
 
 export function extractEnteredNumberValues(form){
-// We are using some DOM/browser APIs like FormData 
-// We are getting the input provided by the user in the form 
   const formData = new FormData(form);
   const numberInputs = extractNumbers(formData);
+  
   return numberInputs;
 }
 
