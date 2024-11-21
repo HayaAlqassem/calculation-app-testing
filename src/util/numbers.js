27e223ee6fe43c0afebab309ffc7bd0123d68f62
Haya <Haya.A.S.Alqassem@gmail.com>
2024-11-21 12:45:53 +0300

    Update numbers.js

--- src/util/numbers.js
+++ src/util/numbers.js
@@ -4,8 +4,6 @@ export function transformToNumber(value) {
   return +value;
 }
 
-// cleanNumbers function is responsible for giving us numbers of the type number.
-// We are going to have integration tests because we are testing a function that calls other functions. 
 export function cleanNumbers(numberValues) { 
   const numbers = [];
   for (const numberInput of numberValues) {
